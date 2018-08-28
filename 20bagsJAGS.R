e9ccf6c71bf70c8ed5f8b47203992e3f512d7e6c
Kurt <kurt2718@users.noreply.github.com>
2018-08-28 22:39:02 +0200

    almost done

---
+++ 20bagsJAGS.R
@@ -0,0 +1,241 @@
+
+
+graphics.off() # This closes all of R's graphics windows.
+rm(list=ls())
+
+require(rjags)
+source('DBDA2E-utilities.R')
+Niter = 1000
+Nburn = 500
+# Zooms the plot label texts
+cx =1.9 
+
+#################################################################################
+
+# Creates the JAGS sample as mcmc.list from the specified model and data
+mcmcf = function(data, params = c("theta","alpha","beta"),model) {
+    
+    jags = jags.model( model,
+                            data = data,
+                            n.chains = 4,
+                            n.adapt = 500)
+    ## Burn-In
+    update(jags, n.iter=Nburn)
+
+    mcmclist = coda.samples (jags,
+                  variable.names = params,
+                  n.iter = Niter,
+                  thin=1)
+}
+#################################################################################
+# Creates a data set of 10 0-bags and 10 1-bags
+
+emptyBags = function() {
+    dataList = list(
+        Nbags = 21 
+    )
+}
+
+uniformBags = function() {
+# All bags are completely uniform
+
+    y = c(rep(1,200),rep(0,200),1)
+    Nbags = 20
+    bags = c()
+    for(bIdx in 1:Nbags) {
+        bags = c(bags, rep(bIdx,20))
+    }
+    Nbags = Nbags + 1
+    bags = c(bags,Nbags)
+
+    dataList = list(
+        y = y,
+        bags = bags,
+        Ntotal = length(y),
+        Nbags = Nbags 
+    ) 
+}
+
+# Creates 20 mixed bags, skewed towards 0-side
+mixedBags = function() {
+
+    Nbags = 20
+
+    y = c()
+    for(i in 1:(Nbags/2)) {
+        add = c(rep(1,i), rep(0,Nbags-i)) 
+        show(length(y))
+        show(add)
+        y = c(y, add, add)
+    }
+    y = c(y,1)
+
+    bags = c()
+    for(bIdx in 1:Nbags) {
+        bags = c(bags, rep(bIdx,20))
+    }
+    Nbags = Nbags + 1
+    bags = c(bags,Nbags)
+
+    dataList = list(
+        y = y,
+        bags = bags,
+        Ntotal = length(y),
+        Nbags = Nbags 
+    )
+}
+
+#################################################################################
+
+# Plots alpha, beta, theta1, theta11, theta21 
+ploth = function(mcmcl, main = "Prior") {
+
+    ylim = c(0,8)
+
+    plot(density(x=log(as.matrix(mcmcl[,'alpha']))),
+         xlim=c(-8,2),
+         ylim=c(0,1),
+         xlab = expression("log("*alpha*")"),
+         ylab = expression("p("*alpha*")"),
+         main = main,
+         cex.lab=cx,
+         col="plum1", lwd=3)
+
+    plot(density(x=as.matrix(mcmcl[,'beta']),
+                 from=0.02,
+                 to=0.98),
+         ylim=ylim,
+         xlab = expression(beta),
+         ylab = expression("p("*beta*")"),
+         main = "",
+         cex.lab=cx,
+         col="plum1", lwd=3)
+
+
+    ## plot(density(x=as.matrix(mcmcl[,'theta[1]']),
+    ##              from=0.02,
+    ##              to=0.98),
+    ##      ylim=ylim,
+    ##      xlab = expression(theta[1]),
+    ##      ylab = expression("p("*theta[1]*")"),
+    ##      main = "",
+    ##      cex.lab=cx,
+    ##      col="skyblue", lwd=3)
+
+    plot(density(x=as.matrix(mcmcl[,'theta[11]']),
+                 from=0.02,
+                 to=0.98),
+         ylim=ylim,
+         xlab = expression(theta[11]),
+         ylab = expression("p("*theta[11]*")"),
+         main = "",
+         cex.lab=cx,
+         col="skyblue", lwd=3)
+    plot(density(x=as.matrix(mcmcl[,'theta[21]']),
+                 from=0.02,
+                 to=0.98),
+         ylim=ylim,
+         xlab = expression(theta[21]),
+         ylab = expression("p("*theta[21]*")"),
+         main = "",
+         cex.lab=cx,
+         col="skyblue",  lwd=3)
+}
+
+plotf = function(mcmcl, main="Prior") {
+
+    ylim = c(0,8)
+
+    plot(density(x=as.matrix(mcmcl[,'theta[1]']),
+                 from=0.02,
+                 to=0.98),
+         xlab = expression(theta[1]),
+         ylab = expression("p("*theta[1]*")"),
+         main = main,
+         cex.lab=cx,
+          ylim=ylim,
+         col="skyblue", lwd=3)
+
+    plot(density(x=as.matrix(mcmcl[,'theta[11]']),
+                 from=0.02,
+                 to=0.98),
+         xlab = expression(theta[11]),
+         ylab = expression("p("*theta[11]*")"),
+         main = "",
+         ylim=ylim,
+         cex.lab=cx,
+         col="skyblue", lwd=3)
+    plot(density(x=as.matrix(mcmcl[,'theta[21]']),
+                 from=0.02,
+                 to=0.98),
+         xlab = expression(theta[21]),
+         ylab = expression("p("*theta[21]*")"),
+         main = "",
+         cex.lab=cx,
+         ylim=ylim,
+         col="skyblue", lwd=3)
+}
+
+#################################################################################
+# Hierarchical Model 
+# Trick Jags into generating priors by providing no data
+hierPri = mcmcf(data = emptyBags(),
+               model = "MultiHierJAGSPriors.bug")
+
+# Hierarchical Posterior w/ Uniform Data
+hierPosUnif = mcmcf(data = uniformBags(),
+                    model = "MultiHierJAGS.bug")
+
+# Hierarchical Posterior w/ Mixed Data
+hierPosMixed = mcmcf(data = mixedBags(),
+                    model = "MultiHierJAGS.bug")
+
+# Flat Model
+flatPri = mcmcf(data = emptyBags(),
+                model = "MultiJAGSPriors.bug")
+
+flatPosUnif = mcmcf(data = uniformBags(),
+                    model = "MultiJAGS.bug")
+
+flatPosMixed = mcmcf(data = mixedBags(),
+                    model = "MultiJAGS.bug")
+#################################################################################
+
+
+openGraph(width=10,height=12)
+## layout(matrix( c(1:20,
+##                  rbind(c(0,0,21,22,23),
+##                        c(0,0,24,25,26)),
+##                  ncol=6)))
+## m = rbind(c( 1, 6,11),
+##           c( 2, 7,12),
+##           c( 3, 8,13),
+##           c( 4, 9,14),
+##           c( 5,10,15))
+m = rbind(c(1,5,9),
+          c(2,6,10),
+          c(3,7,11),
+          c(4,8,12))
+layout(m)
+
+mainUnif = "Posterior, uniform bags."
+mainMix = "Posterior, mixed Bags."
+par(mar=c(4,5,4 ,5))
+ploth(hierPri)
+ploth(hierPosMixed, main=mainMix)
+ploth(hierPosUnif, main=mainUnif)
+
+saveGraph(file="Hierarchical20Bags",type="pdf")
+## # Open a new plot window
+X11()
+
+openGraph(width=10,height=10)
+m = rbind(c(1,4,7),
+          c(2,5,8),
+          c(3,6,9))
+par(mar=c(4,5,4,5))
+layout(m)
+plotf(flatPri)
+plotf(flatPosMixed, main=mainMix)
+plotf(flatPosUnif, main=mainUnif)
+saveGraph(file="Flat20Bags",type="pdf")
--- SimpleBayes.R
+++ SimpleBayes.R
@@ -1,20 +1,28 @@
 # Simple Bayesian model to infer bias of a coin.
 
+source('DBDA2E-utilities.R')
 # Resolution of all parameters. How fine-grained is the grid?
-res = 100 
+res = 1000 
 # Set Unit interval based on resolution
-UnitInt = seq(1/res^2,1-1/res^2,length.out=res) 
+UnitInt = seq(0.01,0.99,length.out=res) 
 
 # Parameters for a beta distribution
-omega = 0.5
-kappa = 4
+alpha = 1 
+beta = c(0.5,0.5) 
 
-a = omega * (kappa - 2) + 1 
-b = (1 - omega) * (kappa - 2) + 1 
+a = alpha*beta[1]
+b = alpha*beta[2]
+## omega = 0.5
+## kappa = 4
+
+## a = omega * (kappa - 2) + 1 
+## b = (1 - omega) * (kappa - 2) + 1 
 
 # the 'Data'
 N = 20 
 z = 17
+N2 = 1
+z2 = 1
 
 # Prior distribution over theta
 Theta = UnitInt # Possible theta values
@@ -25,20 +33,31 @@ pDataGivenTheta = Theta^z * (1 - Theta)^(N-z) # Likelihood
 pData = sum( pDataGivenTheta * pTheta) # In place of an integral 
 pThetaGivenData = pDataGivenTheta * pTheta / pData # Bayes Theorem!
 
+pDataGivenTheta = Theta^z2 * (1 - Theta)^(N2-z2) # Likelihood
+pData = sum( pDataGivenTheta * pTheta) # In place of an integral 
+pThetaGivenData2 = pDataGivenTheta * pTheta / pData # Bayes Theorem!
+
+openGraph(width=5,height=7)
 # Plot the results.
-layout( matrix( c( 1,2,3 ) ,nrow=3 ,ncol=1 ,byrow=FALSE ) ) # 3x1 panels
+layout( matrix( c( 1,2,3 ) ,ncol=1 ,byrow=FALSE ) ) # 3x1 panels
 par( mar=c(3,3,1,0) , mgp=c(2,0.7,0) , mai=c(0.5,0.5,0.3,0.1) ) # margins
 
 plotType = "l" # Plot Bars
 
-plot( Theta , pTheta , type=plotType , lwd=3,
-     yaxt='n', xlab=bquote(theta) , ylab=bquote(p(theta)),
+plot( Theta , dThetaBeta, type=plotType , lwd=3,
+     xlab=bquote(theta) , ylab=bquote(p(theta)),
+     yaxt='n',
+     xlim=c(0.02,0.98),
+     ylim=c(0,8),
      main="Prior" , col="plum1" )
-plot( Theta , pDataGivenTheta , type=plotType , lwd=3,
-     yaxt='n', xlab=bquote(theta) , ylab=bquote( "p(D|" * theta * ")" ) ,
-     main="Likelihood" , col="plum1" )
+plot( Theta, pThetaGivenData2, type=plotType , lwd=3,
+     yaxt='n', xlab=bquote(theta) , ylab=bquote( "p(" * theta * ")" ) ,
+     main="Posterior for N=1, z=1" , col="plum1" )
+abline(lwd=3, v=weighted.mean(Theta,pThetaGivenData2), col='plum1')
 plot( Theta, pThetaGivenData, type=plotType , lwd=3,
-     yaxt='n', xlab=bquote(theta) , ylab=bquote( "p(" * theta * "|D)" ) ,
-     main="Posterior" , col="plum1" )
+     yaxt='n', xlab=bquote(theta) , ylab=bquote( "p(" * theta * ")" ) ,
+     main="Posterior for N=20, z=17" , col="plum1" )
 abline(lwd=3, v=weighted.mean(Theta,pThetaGivenData), col='plum1')
- 
+weighted.mean(Theta,pThetaGivenData)
+
+saveGraph(file="SimpleBayes",type="pdf")
